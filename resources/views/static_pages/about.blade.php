30359dfd47011ae07babbb65132c71d76dda9a40
wr <www@c232996b4298>
2019-10-27 03:34:51 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('title')
+  关于我
+@stop
+@section('content')
+  <h1>关于我</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//GET 常用于页面读取 //POST 常用于数据提交 //PATCH 常用于数据更新 //DELETE 常用于数据删除
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','StaticPagesController@home');
+Route::get('/help','StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
