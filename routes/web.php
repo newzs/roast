ab74beee9a3a796806e445af539bc659b49d5572
wr <www@c232996b4298>
2019-10-29 02:48:29 +0000

    route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 //GET 常用于页面读取 //POST 常用于数据提交 //PATCH 常用于数据更新 //DELETE 常用于数据删除
 
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/','StaticPagesController@home')->name('home');;
+Route::get('/help','StaticPagesController@help')->name('help');;
+Route::get('/about', 'StaticPagesController@about')->name('about');
