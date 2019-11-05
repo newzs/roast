f0f2baab2c3e4e20aad822adc6b27af89a885edb
wr <www@c232996b4298>
2019-11-05 08:00:27 +0000

    destory user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,8 +17,9 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = '112112';
+        $user->email = '112112@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
